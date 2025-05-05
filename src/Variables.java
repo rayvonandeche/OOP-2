00ff779fb7378c0609ff814cf5cf63d4575ab840
rayvonandeche <andecheray23@gmail.com>
2025-05-05 15:00:26 +0300

    challenge 2

--- src/Variables.java
+++ src/Variables.java
@@ -1,3 +1,19 @@
 public class Variables {
+    void showInterest() {
+        double amount;
+        double interest;
 
+        amount = 1200.00;
+        System.out.println("The amount is: " + amount);
+
+        interest = 3;
+        amount = amount * (100 + interest) / 100;
+        System.out.println("The amount after a year is: " + amount);
+
+
+    }
+
+    public static void main(String[] args) {
+        (new Variables()).showInterest();
+    }
 }
